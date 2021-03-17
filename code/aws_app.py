299d52cf5ca33f065340ee32ed59a111d095dc53
PeanutZiggy <kitov.yordan@gmail.com>
2021-03-17 23:41:38 +0200

    can display and delete all reminders

--- code/aws_app.py
+++ code/aws_app.py
@@ -5,12 +5,10 @@ client = boto3.resource(
     's3',
     aws_access_key_id='AKIA44KNG4E7HWKSP4UV',
     aws_secret_access_key='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',
-
 )
 
 
 def create_bucket(bucket_prefix, s3_connection):
-
     bucket_name = bucket_prefix
     bucket_response = s3_connection.create_bucket(
         Bucket=bucket_name,
@@ -41,10 +39,20 @@ def create_temp_file(file_name):
 
 
 def upload_reminder():
-
     tmp_file = create_temp_file("test3")
 
     upload_file("firstpybucket", tmp_file)
 
 
-upload_reminder()
+def delete_reminder(from_bucket, file_name):
+    client.Object(from_bucket, file_name).delete()
+
+
+def delete_all_reminders(from_bucket):
+    for obj in client.Bucket(from_bucket).objects.all():
+        client.Object(from_bucket, obj.key).delete()
+
+
+def display_all_reminders(from_bucket):
+    for obj in client.Bucket(from_bucket).objects.all():
+        print(obj.key)
