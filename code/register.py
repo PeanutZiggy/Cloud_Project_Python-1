5ba369132e4e87ef4d774a6ddd7f78d2437b6c12
Saffan Ahmed <saffan_a@icloud.com>
2021-03-20 17:58:05 +0000

    Update.

--- code/register.py
+++ code/register.py
@@ -3,15 +3,18 @@
 from tkinter import *
 
 
-def main_scren():
+def main_screen():
     screen = Tk()
     screen.geometry("300x250")
     screen.title("Notes 1.0")
     Label(text="Notes 1.0", bg="grey", width="300",
-          height="2", font=("Calibri", 13)).pack()
+          height="2", font=("Calibri", 14)).pack()
     Label(text="").pack()
     Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
     Button(text="Register", height="2", width="30", command=register).pack()
 
     screen.mainloop()
+
+
+main_screen()
