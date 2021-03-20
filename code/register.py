558954fac197d60261fdfbeb2daf000bce8d7ca1
Saffan Ahmed <saffan_a@icloud.com>
2021-03-20 18:36:01 +0000

    Register User Function and Login Function Re-editted.

--- code/register.py
+++ code/register.py
@@ -2,12 +2,22 @@
 
 from tkinter import *
 
+'''
+Register User Function:
+Creates individual txt files for each user that registers.
+Each txt file created will be named by the username the user has created.
+
+*** works in conjuction with Register function.
+'''
+
 
 def register_user():
+    print("working")
+
     username_info = username.get()
     password_info = password.get()
 
-    file = open(username_info+".txt", "w")
+    file = open(username_info, "w")
     file.write(username_info+"\n")
     file.write(password_info)
     file.close()
@@ -17,6 +27,34 @@ def register_user():
 
     Label(screen1, text="Registration Sucess",
           fg="green", font=("calibri", 11)).pack()
+
+
+def login_verify():
+
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+        else:
+            password_not_recognised()
+
+    else:
+        user_not_found()
+
+
+'''
+Register Function:
+Once user selects the Register button and fills in the form by creating a username and password, 
+it will then create an instance of those credentials within a txt file. That is found in the
+'User Details Test' folder.
+'''
 
 
 def register():
@@ -32,9 +70,10 @@ def register():
     username = StringVar()
     password = StringVar()
 
-    Label(screen1, text="Registration Details: ").pack()
+    Label(screen1, text="Please enter details below").pack()
     Label(screen1, text="").pack()
     Label(screen1, text="Username * ").pack()
+
     username_entry = Entry(screen1, textvariable=username)
     username_entry.pack()
     Label(screen1, text="Password * ").pack()
@@ -46,7 +85,32 @@ def register():
 
 
 def login():
-    print("Login session started!")
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("300x250")
+    Label(screen2, text="Please enter details below to login").pack()
+    Label(screen2, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_entry1
+    global password_entry1
+
+    Label(screen2, text="Username * ").pack()
+    username_entry1 = Entry(screen2, textvariable=username_verify)
+    username_entry1.pack()
+    Label(screen2, text="").pack()
+    Label(screen2, text="Password * ").pack()
+    password_entry1 = Entry(screen2, textvariable=password_verify)
+    password_entry1.pack()
+    Label(screen2, text="").pack()
+    Button(screen2, text="Login", width=10,
+           height=1, command=login_verify).pack()
 
 
 def main_screen():
@@ -55,7 +119,7 @@ def main_screen():
     screen.geometry("300x250")
     screen.title("Notes 1.0")
     Label(text="Notes 1.0", bg="grey", width="300",
-          height="2", font=("Calibri", 14)).pack()
+          height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
     Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
