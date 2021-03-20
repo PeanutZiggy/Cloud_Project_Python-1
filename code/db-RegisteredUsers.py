2456d11187974f175ff9106f464e38558654b3a4
Saffan Ahmed <saffan_a@icloud.com>
2021-03-20 17:25:50 +0000

    Inserting DataFrame into SQL Server.

--- code/db-RegisteredUsers.py
+++ code/db-RegisteredUsers.py
@@ -1,3 +1,4 @@
+# Prequisite Modules
 import pandas as pd
 import config
 
@@ -21,3 +22,40 @@ print(connectingSQLServer)
 cursor = connectingSQLServer.cursor()
 # Database Entity
 db_name = 'db-registeredusers'
+
+# Creating the Database
+
+
+def database_create(cursor, database):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(database))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+try:
+    cursor.execute("USE {}".format(db_name))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(db_name))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        database_create(cursor, db_name)
+        print("Database {} created successfully.".format(db_name))
+        connectingSQLServer.database = db_name
+    else:
+        print(err)
+        exit(1)
+
+connectingSQLServer.commit()
+cursor.close()
+connectingSQLServer.close()
+
+SQL_Engine = create_engine("mysql+mysqlconnector://{user}:{pw}@{host}/{db}"
+                           .format(user=config.username,
+                                   pw=config.password,
+                                   host=config.host,
+                                   db=config.db_name))
+
+# Inserting entire DataFrame into MySQL Server.
+df.to_sql('Login_Credentials', con=SQL_Engine, if_exists='append')
