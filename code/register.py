5d2d1a3a89cce6697a7754d49387249e5effe085
Saffan Ahmed <saffan_a@icloud.com>
2021-03-20 18:06:00 +0000

    Creating Register Function.

--- code/register.py
+++ code/register.py
@@ -3,6 +3,32 @@
 from tkinter import *
 
 
+def register():
+    global screen1
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(screen1, text="Please enter details below").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username * ").pack()
+    username_entry = Entry(screen1, textvariable=username)
+    username_entry.pack()
+    Label(screen1, text="Password * ").pack()
+    password_entry = Entry(screen1, textvariable=password)
+    password_entry.pack()
+    Label(screen1, text="").pack()
+    Button(screen1, text="Register", width=10,
+           height=1, command=register_user).pack()
+
+
 def main_screen():
     screen = Tk()
     screen.geometry("300x250")
