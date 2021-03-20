783d23f1eb750d1d0ed4f9231e410e8e56179dc5
Saffan Ahmed <saffan_a@icloud.com>
2021-03-20 17:16:57 +0000

    Connecting to MySQL Server.

--- code/db-RegisteredUsers.py
+++ code/db-RegisteredUsers.py
@@ -7,4 +7,17 @@ from mysql.connector import errorcode
 from sqlalchemy import create_engine
 
 # CSV import
-data_frame = pd.read_csv('')
+# Team Members: Change your path accordingly as folders can vary.
+data_frame = pd.read_csv(
+    '/Users/saffanahmed/Documents/Cloud_Project_Python/code/user_credentials.csv')
+
+# SQL Setup: Connecting to the MySQL Server and Creating a Database for Registered Users
+connectingSQLServer = mysql.connector.connect(
+    host=config.host,
+    user=config.username,
+    password=config.password)
+print(connectingSQLServer)
+
+cursor = connectingSQLServer.cursor()
+# Database Entity
+db_name = 'db-registeredusers'
